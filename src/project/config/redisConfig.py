149393dbf0c682e26a0c39763b3111d4cc709ab1
chiya <wei895926300@126.com>
2023-02-10 13:25:26 +0800

    redis序列化修正废弃方法

--- src/project/config/redisConfig.py
+++ src/project/config/redisConfig.py
@@ -21,7 +21,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -33,11 +33,12 @@ import java.time.Duration;
  * redis配置
  */
 @Configuration
-public class BaseRedisConfig implements WebMvcConfigurer{
+public class BaseRedisConfig implements WebMvcConfigurer {
 
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisSerializer<Object> serializer = redisSerializer();
+
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -50,13 +51,11 @@ public class BaseRedisConfig implements WebMvcConfigurer{
 
 	@Bean
 	public RedisSerializer<Object> redisSerializer() {
-		// 创建JSON序列化器
-		Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 		// 必须设置，否则无法将JSON转化为对象，会转化成Map类型
 		objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-		serializer.setObjectMapper(objectMapper);
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 		return serializer;
 	}
 
@@ -65,7 +64,7 @@ public class BaseRedisConfig implements WebMvcConfigurer{
 		RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
 		// 设置Redis缓存有效期为1天
 		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer())).entryTtl(Duration.ofDays(1));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer())).entryTtl(Duration.ofDays(1));
 		return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
 	}
 
